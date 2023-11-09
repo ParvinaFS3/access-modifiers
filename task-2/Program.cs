b010720e3e43d8e20c7fc31fe56acd9d204a2b07
ParvinaFS3 <parvinaisma@gmail.com>
2023-11-10 00:09:28 +0400

    first commit

---
+++ task-2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+
+namespace task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st = new Student();
+            st.Grades = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            Console.WriteLine(string.Join(", ", st.Grades)); // Çıxış: 2, 4, 6, 8
+        }
+        class Student
+        {
+            private int[] grades;
+
+            public int[] Grades
+            {
+                get { return grades; }
+                set { grades = value.Where(x => x % 2 == 0).ToArray(); }
+            }
+        }
+    }
+}
