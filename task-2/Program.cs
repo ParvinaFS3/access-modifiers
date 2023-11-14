a6a7e28fb717c2c0b1a1369c37daa688cda9d228
ParvinaFS3 <parvinaisma@gmail.com>
2023-11-14 23:03:27 +0400

    first commit

--- task-2/Program.cs
+++ task-2/Program.cs
@@ -1,27 +1,127 @@
 ﻿using System;
-using System.Linq;
 
-
-namespace task_2
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Student st = new Student();
-            st.Grades = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+        
+        User[] users = {
+            new Student("Aslanova", "Nargiz", "nargiz_lan", "password123", 10),
+            
+        };
 
-            Console.WriteLine(string.Join(", ", st.Grades)); // Çıxış: 2, 4, 6, 8
-        }
-        class Student
-        {
-            private int[] grades;
+        Console.Write("Enter username: ");
+        string enteredUsername = Console.ReadLine();
+        Console.Write("Enter password: ");
+        string enteredPassword = Console.ReadLine();
 
-            public int[] Grades
+        
+        User authenticatedUser = null;
+        foreach (var user in users)
+        {
+            if (user.Username == enteredUsername && user.Password == enteredPassword)
             {
-                get { return grades; }
-                set { grades = value.Where(x => x % 2 == 0).ToArray(); }
+                authenticatedUser = user;
+                break;
             }
         }
+        // Əgər istifadəçi tapılıbsa və istifadəçi bir tələbədirsə Grade-i ekrana çap et
+        if (authenticatedUser is Student student)
+        {
+            Console.WriteLine($"Welcome, {student.Name} {student.Surname}!");
+            Console.WriteLine($"Your Grade: {student.Grade}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid username or password.");
+        }
+    }
+
+}
+class User
+{
+    public string Name { get; }
+    public string Surname { get; }
+    public string Username { get; }
+    public string Password { get; }
+
+    public User(string name, string surname, string username, string password)
+    {
+        Name = name;
+        Surname = surname;
+        Username = username;
+        Password = password;
     }
 }
+
+
+class Student : User
+{
+    public int Grade { get; }
+
+    public Student(string name, string surname, string username, string password, int grade)
+        : base(name, surname, username, password)
+    {
+        Grade = grade;
+    }
+}
+
+
+
+
+/*using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr1 = { 2, 3, 4, 7 };
+        int[] arr2 = { 3, 2, 7 };
+
+        bool result = IsArrayContains(arr1, arr2);
+        Console.WriteLine(result); // Output: False
+
+        int[] arr3 = { 2, 3, 4, 7 };
+        int[] arr4 = { 3, 4 };
+
+        bool result2 = IsArrayContains(arr3, arr4);
+        Console.WriteLine(result2); // Output: True
+    }
+
+    static bool IsArrayContains(int[] array1, int[] array2)
+    {
+        if (array1.Length < array2.Length)
+        {
+            return false;
+        }
+
+        // İkinci arrayin bütün elementləri ilə birinci arrayin elementlərini müqayisə et
+        for (int i = 0; i <= array1.Length - array2.Length; i++)
+        {
+            if (IsSubArray(array1, array2, i))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    static bool IsSubArray(int[] array1, int[] array2, int startIndex)
+    {
+        for (int i = 0; i < array2.Length; i++)
+        {
+            if (array1[startIndex + i] != array2[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}*/
+
+
+
+
+
+
